1cec99d492b43e80e2ab184f6f913fe01531a9cf
BruceHi <BruceHi@users.github.com>
2020-03-23 21:08:34 +0800

    change a little isPalindrome.py and test3.py

--- test3.py
+++ test3.py
@@ -12,3 +12,17 @@ a = ['a', 'b', 'c']
 # b = {}
 b = ''
 print(list(zip(a, b)))
+
+a = 0
+b = 1
+a, b = b, a+b
+print(a, b)
+
+*b, = {'weight': 50, 'name': 'Bob', 'age': 20}
+print(b)
+*d, = 1, 2, 3
+print(type(d))
+
+list1 = ['Google', 'Runoob', 'Taobao']
+list1.insert(1, 'Baidu')
+print ('列表插入元素后为 : ', list1)
