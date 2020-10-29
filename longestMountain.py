a1b996d98af6f2f43d7711f51dcf1f55ee9bf85e
BruceHi <BruceHi@users.github.com>
2020-10-29 19:56:23 +0800

    add longestMountain

--- longestMountain.py
+++ longestMountain.py
@@ -3,21 +3,49 @@ from typing import List
 
 
 class Solution:
+    # # 失败，遇见 最高处相等，2, 3, 3, 2, 0, 2 就不行。
+    # def longestMountain(self, A: List[int]) -> int:
+    #     n = len(A)
+    #     if not n or n == 1:
+    #         return 0
+    #     left = float('inf')
+    #     if A[0] < A[1]:
+    #         left = 0
+    #     res = 0
+    #     for i in range(1, n-1):
+    #         if A[i-1] > A[i] < A[i+1]:
+    #             res = max(res, i - left + 1)
+    #             left = i
+    #         elif A[i-1] == A[i]:
+    #             left = i
+    #         elif A[i] == A[i+1]:
+    #             left = i + 1
+    #     if A[-1] < A[-2]:
+    #         res = max(res, n - left)
+    #     return res
+
+    # 开头和末尾无法做为山顶，但可以做为山脚。
     def longestMountain(self, A: List[int]) -> int:
-        n = len(A)
-        if not n or n == 1:
+        if not A:
             return 0
-        left = float('inf')
-        if A[0] < A[1]:
-            left = 0
+
+        n = len(A)
+        left = [0] * n
+        for i in range(1, n):
+            if A[i-1] < A[i]:
+                left[i] = left[i-1] + 1
+
+        right = [0] * n
+        for i in range(n-2, -1, -1):
+            if A[i] > A[i+1]:
+                right[i] = right[i+1] + 1
+
         res = 0
-        for i in range(1, n-1):
-            if A[i-1] > A[i] < A[i+1] or A[i-1] == A[i] < A[i+1] or A[i-1] > A[i] == A[i+1]:
-                res = max(res, i - left + 1)
-                left = i
-        if A[-1] < A[-2]:
-            res = max(res, n - left)
+        for i in range(n):
+            if left[i] and right[i]:
+                res = max(res, left[i] + right[i] + 1)
         return res
+
 
 
 s = Solution()
@@ -30,3 +58,7 @@ print(s.longestMountain([]))
 print(s.longestMountain([1,1,0,0,1,0]))
 
 print(s.longestMountain([1,2,2,2]))
+
+print(s.longestMountain([2, 0, 2, 2, 3]))
+
+print(s.longestMountain([2, 3, 3, 2, 0, 2]))
