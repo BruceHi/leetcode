56b96bd98da7e97f557372a20ee22a5155a40b80
BruceHi <BruceHi@users.github.com>
2021-06-05 10:04:33 +0800

    commit 6.5

--- LinkedList/10removeElements.py
+++ LinkedList/10removeElements.py
@@ -44,6 +44,18 @@ class Solution:
     #         cur = cur.next
     #     return dummy.next
 
+    # def removeElements(self, head: ListNode, val: int) -> ListNode:
+    #     pre = dummy = ListNode(0)
+    #     dummy.next = head
+    #
+    #     while pre.next:
+    #         cur = pre.next
+    #         if cur.val == val:
+    #             pre.next = cur.next
+    #         else:
+    #             pre = pre.next
+    #     return dummy.next
+
     def removeElements(self, head: ListNode, val: int) -> ListNode:
         pre = dummy = ListNode(0)
         dummy.next = head
@@ -55,7 +67,6 @@ class Solution:
             else:
                 pre = pre.next
         return dummy.next
-
 
 def generate_link(nums: List[int]) -> ListNode:
     head = ListNode(0)
--- LinkedList/9getIntersectionNode.py
+++ LinkedList/9getIntersectionNode.py
@@ -40,6 +40,13 @@ class Solution:
     #         cur2 = cur2.next if cur2 else headA
     #     return cur1
 
+    # def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+    #     cur1, cur2 = headA, headB
+    #     while cur1 != cur2:
+    #         cur1 = cur1.next if cur1 else headB
+    #         cur2 = cur2.next if cur2 else headA
+    #     return cur1
+
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         cur1, cur2 = headA, headB
         while cur1 != cur2:
